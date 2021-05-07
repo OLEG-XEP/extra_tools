8e436395f87caca38445e733d0d92023c98717ef
OLEG-XEP <papademon1977@gmail.com>
2021-05-07 17:05:09 +0300

    Add binaries folder

---
+++ bins/smd_down.sh
@@ -0,0 +1,4 @@
+#!/system/bin/sh
+
+
+echo 0 > /sys/module/hci_smd/parameters/hcismd_set
---
+++ bins/smd_up.sh
@@ -0,0 +1,5 @@
+#!/system/bin/sh
+
+
+echo 0 > /sys/module/hci_smd/parameters/hcismd_set
+echo 1 > /sys/module/hci_smd/parameters/hcismd_set
